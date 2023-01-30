4f487dee6b2155b4ab50f63ca04b807c54c4b16d
土岐田力輝 <tokitachikaraakira@mbp.local>
2023-01-30 21:53:23 +0900

    hel

--- main.py
+++ main.py
@@ -6,29 +6,33 @@ def redirect(_url):
     st.markdown(link, unsafe_allow_html=True)
 st.title("PrintGraph紹介サイト")
 st.subheader("コードがわかるグラフ作成ツール")
-start=st.button('START')
-st.caption('アプリケーションを開始します．')
-col = st.columns(6)
-proposal = col[0].button('企画書')
-col[0].caption('アプリ開発の目的，構想，詳細などの情報がまとめられています．')
-method = col[2].button('紹介動画')
-col[2].caption('アプリの使い方についての説明動画です．')
-git = col[4].button('Github')
-col[4].caption('アプリのプログラムです．')
-manual = col[5].button("マニュアル")
-col[5].caption("使い方のマニュアルです．")
+# start=st.button('START')
+# st.caption('アプリケーションを開始します．')
+# col = st.columns(6)
+# proposal = col[0].button('企画書')
+# col[0].caption('アプリ開発の目的，構想，詳細などの情報がまとめられています．')
+# method = col[2].button('紹介動画')
+# col[2].caption('アプリの使い方についての説明動画です．')
+# git = col[4].button('Github')
+# col[4].caption('アプリのプログラムです．')
+# manual = col[5].button("マニュアル")
+# col[5].caption("使い方のマニュアルです．")
 
-if start:
-    webbrowser.open("https://dancing-clafoutis-499aab.netlify.app/")
-if proposal:
-    webbrowser.open('https://drive.google.com/file/d/119abdlHGUJ9PqECeyT7Aitc5JGY-qy-5/view?usp=sharing')
-if method:
-    webbrowser.open("https://youtu.be/_4r3IcXitPw")
-if git:
-    webbrowser.open('https://github.com/azuma-naoki/PriGraph')
-if manual:
-    webbrowser.open("https://drive.google.com/file/d/1B5Z_DaZXv7rHu_aVev9Db8xOAVSRgXYC/view?usp=sharing")
 
+link = '[アプリケーション](https://dancing-clafoutis-499aab.netlify.app/)'
+st.markdown(link, unsafe_allow_html=False)
+
+link = '[企画書](https://drive.google.com/file/d/119abdlHGUJ9PqECeyT7Aitc5JGY-qy-5/view?usp=sharing)'
+st.markdown(link, unsafe_allow_html=False)
+
+link = '[紹介動画](https://youtu.be/_4r3IcXitPw)'
+st.markdown(link, unsafe_allow_html=False)
+
+link = '[GitHub](https://github.com/azuma-naoki/PriGraph)'
+st.markdown(link, unsafe_allow_html=False)
+
+link = '[マニュアル](https://drive.google.com/file/d/1B5Z_DaZXv7rHu_aVev9Db8xOAVSRgXYC/view?usp=sharing)'
+st.markdown(link, unsafe_allow_html=False)
 
 components.html(
     """    
